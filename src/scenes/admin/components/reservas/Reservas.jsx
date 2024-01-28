67451de72f668aff98b937574e5f14a5de287819
mvillanueva24 <mariorvillanueva24@gmail.com>
2024-01-28 16:34:28 -0500

    Preloader and Submenu Icons

--- src/scenes/admin/components/reservas/Reservas.jsx
+++ src/scenes/admin/components/reservas/Reservas.jsx
@@ -12,69 +12,29 @@ import DebouncedInput from "../DebouncedInput";
 import { BiSearchAlt2 } from "react-icons/bi";
 import { useEffect } from "react";
 import Modal from "../../../body/asignaturas/modal";
+import { RxCheck, RxCross2 } from "react-icons/rx";
+import { LuInfo } from "react-icons/lu";
+import { useLoaderData, useNavigate, useOutlet } from "react-router-dom";
 
 const domain_url = import.meta.env.VITE_DOMAIN_DB;
 
 const Reservas = () => {
   // Reservas
-  const [data, setData] = useState([]);
-  const [libros, setLibros] = useState([]);
-  const [alumnos, setAlumno] = useState([]);
-
-  const [isLoading, setIsLoading] = useState(true);
 
   // Modal Data
   const [reservaModal, setReservaModal] = useState(false);
   const [libroCodigo, setLibroCodigo] = useState("");
   const [dniUser, setDniUser] = useState("");
 
-  // RESERVAS
-  useEffect(() => {
-    async function fetchAsignaturas() {
-      const response = await fetch(`${domain_url}/api/reservas`, {
-        method: "GET",
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-        },
-      });
-      const data = await response.json();
-      setData(data);
-      setIsLoading(false);
-    }
-    fetchAsignaturas();
-  }, []);
+  // Accept and Cancel Modal
+  const [acceptModal, setAcceptModal] = useState(false);
+  const [cancelModal, setCancelModal] = useState(false);
+  const [User, setUser] = useState("");
+  const [Libro, setLibro] = useState("");
+  const [diasPrestamo, setDiasPrestamo] = useState("");
+  const [idReserva, setIdReserva] = useState("");
 
-  // LIBROS
-  useEffect(() => {
-    async function fetchLibros() {
-      const response = await fetch(`${domain_url}/api/libros_admin`, {
-        method: "GET",
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-        },
-      });
-      const libros = await response.json();
-      setLibros(libros);
-      setIsLoading(false);
-    }
-    fetchLibros();
-  }, []);
-
-  // ALUMNOS
-  useEffect(() => {
-    async function fetchLibros() {
-      const response = await fetch(`${domain_url}/api/alumnos`, {
-        method: "GET",
-        headers: {
-          "ngrok-skip-browser-warning": "69420",
-        },
-      });
-      const alumnos = await response.json();
-      setAlumno(alumnos);
-      setIsLoading(false);
-    }
-    fetchLibros();
-  }, []);
+  // const [data, setData] = useState([])
 
   const handleLibroCodigoChange = (event) => {
     setLibroCodigo(event.target.value);
@@ -85,6 +45,29 @@ const Reservas = () => {
     setDniUser(event.target.value);
     //setFechaDevolucion(fechaEntrega);
   };
+
+  const data = useLoaderData();
+
+  // useEffect(() => {
+  //   // Cargar la lista de libros
+  //   async function fetchLibros() {
+  //     const response = await fetch(`${domain_url}/api/reservas`, {
+  //       method: "GET",
+  //       headers: {
+  //         "ngrok-skip-browser-warning": "69420", // Puedes agregar más encabezados según sea necesario
+  //       },
+  //     });
+
+  //     const data = await response.json();
+
+  //     setData(data);
+  //     setIsLoading(false);
+  //   }
+
+  //   fetchLibros();
+  // }, []);
+
+  const navigate = useNavigate();
 
   async function newReserva(libro, dni) {
     const data = {
@@ -102,8 +85,26 @@ const Reservas = () => {
     if (!response.ok) {
       throw new Error("Algo salió mal :c");
     }
+  }
 
-    // Actualizar la lista de asignaturas
+  async function acceptOrCancelReserva(idReserva, decision) {
+    const data = {
+      id: idReserva,
+      decision: decision,
+    };
+    console.log(data);
+    const response = await fetch(`${domain_url}/api/reservas_decision`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "ngrok-skip-browser-warning": "69420",
+      },
+      body: JSON.stringify(data),
+    });
+    if (!response.ok) {
+      throw new Error("Algo salió mal :c");
+    }
+    navigate("/admin/reservas");
   }
 
   const columnHelper = createColumnHelper();
@@ -124,21 +125,41 @@ const Reservas = () => {
       header: "Libro",
     }),
     columnHelper.accessor("nombreAlumno", {
-      cell: (info) => <span>{info.getValue()}</span>,
-      // cell: (info) => {
-      //   const encontrarAlumno = alumnos.find((alumno) => alumno.id === info.getValue())
-      //   const alumnoNombre = encontrarAlumno ? encontrarAlumno.nombre : null;
-      //   const alumnoApellido = encontrarAlumno ? encontrarAlumno.apellido : null;
-      //   return <span>{alumnoNombre+", "+alumnoApellido}</span>
+      cell: (info) => (
+        <span className="flex ">
+          <div className="grow">{info.getValue()} </div>
+          <div className="relative group">
+            {info.row.original.demoraAlumno == 0 ? (
+              <div>
+                <button className="bg-green-600 text-white p-1 rounded-full">
+                  <LuInfo size={20} />
+                </button>
+                <div className="hidden group-hover:block w-30 absolute bottom-full left-1/2 transform -translate-x-1/2 p-2 bg-green-300 shadow-lg rounded-lg">
+                  <span>Último préstamo: Sin demoras</span>
+                </div>
+              </div>
+            ) : (
+              <div>
+                <button className="bg-red-600 text-white p-1 rounded-full">
+                  <LuInfo size={20} />
+                </button>
+                <div className="hidden group-hover:block w-30 absolute bottom-full left-1/2 transform -translate-x-1/2 p-2 bg-red-300 shadow-lg rounded-lg cursor-not-allowed">
+                  <span>
+                    Último préstamo: {info.row.original.demoraAlumno} dias de
+                    demora
+                  </span>
+                </div>
+              </div>
+            )}
+          </div>
+        </span>
+      ),
+
       header: "Alumno",
     }),
-    columnHelper.accessor("fecha_entrega", {
-      cell: (info) => <span>{info.getValue()}</span>,
-      header: "Fecha de entrega",
-    }),
-    columnHelper.accessor("fecha_devolucion", {
-      cell: (info) => <span>{info.getValue()}</span>,
-      header: "Fecha de devolucion",
+    columnHelper.accessor("dias_prestamo", {
+      cell: (info) => <span>{info.getValue()} dias</span>,
+      header: "Dias de préstamo",
     }),
   ];
 
@@ -158,7 +179,6 @@ const Reservas = () => {
   return (
     <div className="p-2 w-full  fill-gray-400 ">
       <h3 className="text-2xl">Reservas</h3>
-      {console.log(data)}
       {/* INICIO BUSCADOR */}
       <div className="mt-5 flex justify-between mb-2">
         <div className="w-full flex items-center gap-1">
@@ -191,6 +211,7 @@ const Reservas = () => {
                   )}
                 </th>
               ))}
+              <th>Acciones</th>
             </tr>
           ))}
         </thead>
@@ -199,7 +220,7 @@ const Reservas = () => {
             table.getRowModel().rows.map((row, i) => (
               <tr
                 key={row.id}
-                className={`
+                className={` 
                 ${i % 2 === 0 ? "bg-gray-200" : "bg-gray-300"}
                 `}
               >
@@ -208,6 +229,32 @@ const Reservas = () => {
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </td>
                 ))}
+                <td className="flex gap-6 mt-1">
+                  <button
+                    className="bg-green-600 text-white p-2 rounded-full"
+                    onClick={() => {
+                      setLibro(row.original.nombreLibro);
+                      setUser(row.original.nombreAlumno);
+                      setDiasPrestamo(row.original.dias_prestamo);
+                      setIdReserva(row.original.id);
+                      setAcceptModal(true);
+                    }}
+                  >
+                    <RxCheck size={20} />
+                  </button>
+                  <button
+                    className="bg-red-500 text-white p-2 rounded-full"
+                    onClick={() => {
+                      setLibro(row.original.nombreLibro);
+                      setUser(row.original.nombreAlumno);
+                      setDiasPrestamo(row.original.dias_prestamo);
+                      setIdReserva(row.original.id);
+                      setCancelModal(true);
+                    }}
+                  >
+                    <RxCross2 size={20} />
+                  </button>
+                </td>
               </tr>
             ))
           ) : (
@@ -248,14 +295,14 @@ const Reservas = () => {
       </div>
       <Modal isVisible={reservaModal} onClose={() => setReservaModal(false)}>
         <div className=" p-6">
-          <h3 className="text-xl font-semibold">Crear Asignatura</h3>
+          <h3 className="text-xl font-semibold">Crear Reserva</h3>
           <form
             onSubmit={(e) => {
               e.preventDefault();
               setDniUser("");
               setLibroCodigo("");
               newReserva(libroCodigo, dniUser);
-              setShowModalAdd(false);
+              setReservaModal(false);
             }}
             method="post"
             className="mt-4 flex flex-col"
@@ -286,8 +333,122 @@ const Reservas = () => {
           </form>
         </div>
       </Modal>
+
+      {/* Accept Modal */}
+      <Modal isVisible={acceptModal} onClose={() => setAcceptModal(false)}>
+        <div className=" p-6">
+          <h3 className="text-xl font-semibold">Aceptar Reserva</h3>
+          <h3 className="mt-5 text-lg text-center font-semibold">
+            ¿Desea aceptar esta reserva?
+          </h3>
+          {console.log(idReserva)}
+          <div className="mt-2">
+            <h4 className="text-base font-semibold">Datos:</h4>
+            <p claseName="text-base">Libro: {Libro}</p>
+            <p claseName="text-base">Pedido por: {User}</p>
+            <p claseName="text-base">Días de préstamo: {diasPrestamo}</p>
+          </div>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setLibro("");
+              setUser("");
+              setDiasPrestamo("");
+              setIdReserva("");
+              acceptOrCancelReserva(idReserva, true);
+              setAcceptModal(false);
+            }}
+            action=""
+            method="post"
+            className="mt-8 grid grid-cols-2 justify-items-stretch gap-7 w-full "
+          >
+            <button
+              type="button"
+              className=" bg-gray-500 text-white p-2 hover:bg-gray-400 rounded-md"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="bg-green-600 text-white p-2 hover:bg-green-400 rounded-md"
+            >
+              Aceptar
+            </button>
+          </form>
+        </div>
+      </Modal>
+
+      {/* Cancel Modal */}
+      <Modal isVisible={cancelModal} onClose={() => setCancelModal(false)}>
+        <div className=" p-6">
+          <h3 className="text-xl font-semibold">Denegar Préstamo</h3>
+          <h3 className="mt-5 text-lg text-center font-semibold">
+            ¿Desea denegar esta reserva?
+          </h3>
+          <div className="mt-2">
+            <h4 className="text-base font-semibold">Datos:</h4>
+            <p claseName="text-base">Libro: {Libro}</p>
+            <p claseName="text-base">Pedido por: {User}</p>
+            <p claseName="text-base">Días de préstamo: {diasPrestamo}</p>
+          </div>
+
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              setLibro("");
+              setUser("");
+              setDiasPrestamo("");
+              setIdReserva("");
+              acceptOrCancelReserva(idReserva, false);
+              setCancelModal(false);
+            }}
+            action=""
+            method="post"
+            className="mt-8 grid grid-cols-2 justify-items-stretch gap-7 w-full "
+          >
+            <button
+              type="button"
+              className=" bg-gray-500 text-white p-2 hover:bg-gray-400 rounded-md"
+            >
+              Cancelar
+            </button>
+            <button
+              type="submit"
+              className="bg-red-700 text-white p-2 hover:bg-red-400 rounded-md"
+            >
+              Denegar
+            </button>
+          </form>
+        </div>
+      </Modal>
     </div>
   );
 };
 
 export default Reservas;
+
+export const reservaLoader = async () => {
+  // await fakeNetwork();
+  const response = await fetch(`${domain_url}/api/reservas`, {
+    method: "GET",
+    headers: {
+      "ngrok-skip-browser-warning": "69420", // Puedes agregar más encabezados según sea necesario
+    },
+  });
+
+  // Log the response object to check if it's valid
+  console.log("Response object:", response);
+
+  // Check if the response is OK (status code 200-299)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  // Parse the response as JSON
+  const data = await response.json();
+
+  // Log the parsed data to check if it's valid
+  console.log("Parsed data:", data);
+
+  return data;
+};
